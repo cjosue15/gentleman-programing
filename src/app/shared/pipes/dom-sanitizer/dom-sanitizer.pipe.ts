09a301e9a2b6405f4226e5bc0f328e00fd46faec
Carlos Morales <carlosjosuemn98@gmail.com>
2021-04-12 09:57:08 -0500

    feate(pipe): domsanitizer

---
+++ src/app/shared/pipes/dom-sanitizer/dom-sanitizer.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+@Pipe({
+  name: 'domSanitizer',
+})
+export class DomSanitizerPipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {}
+
+  transform(url: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
