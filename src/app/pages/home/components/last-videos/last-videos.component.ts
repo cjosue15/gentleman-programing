009c57624dcfd434b0da4f8742e361005b613a03
Carlos Morales <carlosjosuemn98@gmail.com>
2021-04-13 10:24:32 -0500

    refactor(observable): catch error

--- src/app/pages/home/components/last-videos/last-videos.component.ts
+++ src/app/pages/home/components/last-videos/last-videos.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Youtube } from '@core/models/youtube';
 import { YoutubeService } from '@shared/services/youtube.service';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-last-videos',
@@ -11,17 +12,20 @@ import { Observable } from 'rxjs';
 export class LastVideosComponent implements OnInit {
   lastVideos: Youtube[];
   lastVideos$: Observable<Youtube[]>;
+  error: string;
 
   constructor(private youtubeService: YoutubeService) {
     this.lastVideos = [];
     this.lastVideos$ = new Observable();
+    this.error = '';
   }
 
   ngOnInit(): void {
-    this.lastVideos$ = this.youtubeService.getLastVideos();
-    this.youtubeService.getLastVideos().subscribe(
-      (response) => (this.lastVideos = response),
-      (error) => console.log(error)
+    this.lastVideos$ = this.youtubeService.getLastVideos().pipe(
+      catchError((error) => {
+        this.error = error.message;
+        return throwError(error.message);
+      })
     );
   }
 }
